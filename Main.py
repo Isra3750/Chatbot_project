7b84321fb8487d3e3c870e636a9646608024c68d
Isra <Israchokwatana@gmail.com>
2024-12-04 21:27:01 +0700

    add documentations notes

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
+# This project will be use for a database chat bot - locally ran oracle database on a docker container. Enterprise edition 19c. Will be using oracledb package and openAI API.
+# Will use NLP to query and push DDL towards the database. ALong with regular data modeling checks to vertify that each push is correct and fully compliance towards best pratices.
+# Streamlit and langchain is to be used somewhere...
+
 import pandas as pd
 
 print('Hello')
