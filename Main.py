0bea419548f8e41b591c60d865e59aae1663a4d5
Isra <Israchokwatana@gmail.com>
2024-12-12 21:22:00 +0700

    revise main file

--- Main.py
+++ Main.py
@@ -8,12 +8,15 @@
 # pip install groq for LLM API
 # pip install langchain-groq for langchain usage with groq API
 # Import OS to get Key from env
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from groq import Groq
-import os
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 # pip install streamlit, Import streamlit for an UI interface
 import streamlit as st
+import os
 
 # load env variable
 load_dotenv()
@@ -31,12 +34,64 @@ st.set_page_config(page_title="Streaming Chatbot v1", page_icon="🤖")
 st.title("Streaming Chatbot")
 st.write("Ask questions about your question to get started!")
 
+
+###This is written third (for LLM response and streaming)
+
+# Get response -> Use LLM to get response
+def get_response(user_query, chat_history):
+    # Template 
+    template = """
+    You are a helpful assistant. Answer the following questions considering the history of the conversation:
+
+    Chat history: {chat_history}
+
+    User question: {user_question}
+    """
+    # Create prompt template - from langchain core 
+    prompt = ChatPromptTemplate.from_template(template)
+
+    # lang chain groq for LLM model API, api key is in env
+    llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile") 
+    
+    # Create chain which output LLM response via StrOutputParser, which is then return
+    chain = prompt | llm | StrOutputParser()
+    
+    # Invoke your chain, with the 2 arg from this function, StrOutputParser will output LLM response
+    # use chain.invoke for one time output, chain.stream for generator output (one at a time output)
+    return chain.stream({
+        "chat_history": chat_history,
+        "user_question": user_query,
+    })
+
+###This is written second (for session history)
+# Conversation, this will create the convo structure, layer the texts box during chats
+for message in st.session_state.chat_history:
+    # human to be icon with human, check if message is human or not
+    if isinstance(message, HumanMessage):
+        with st.chat_message("Human"): # st.chatmessage to select icon
+            st.markdown(message.content) # this will write it out
+    else:
+        with st.chat_message("AI"): # st.chatmessage to select icon
+            st.markdown(message.content) # this will write it out    
+
+###This is written first (User query), ai response was using place holder before this
 # Create the message box with default message
-user_query = st.chat_input("Enter your input...")
+user_query = st.chat_input("Enter your input...")  
 
 # Get user query if it is not an null or empty string
 if user_query is not None and user_query != "":
-    # First, append your user input to chat_history in the session state
-    st.session_state.chat_history.append(user_query)
+    # First, append your user input to chat_history in the session state (as Human message)
+    st.session_state.chat_history.append(HumanMessage(user_query))
+    # show it in streamlit, this will show ur human input in streamlit web UI
+    with st.chat_message("Human"):
+        st.markdown(user_query) # markdown are the chat boxes
+    
+    # This will be the AI response
+    with st.chat_message("AI"):
+        # write it out with markdown (with invoke in function), or write_stream (with stream in function)
+        ai_response = st.write_stream(get_response(user_query, st.session_state.chat_history))
+
+    # Just like the first human append, do the same for AI output
+    st.session_state.chat_history.append(AIMessage(ai_response))
 
 # Run file -> streamlit run Main.py
